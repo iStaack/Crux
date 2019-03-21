7fafaddb862f00ab7ec6c5323b3d41afd61e273d
Isaac Miranda <cmirandam@ucenfotec.ac.cr>
2019-03-20 23:09:22 -0600

    Un poco de avance del inicio de sesion

---
+++ proyecto_individual/api/componentes/usuarios/usuarios.api.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+
+
+
+
+/*
+* let mailOptions = {
+                from: 'amejixteam@gmail.com',
+                to: nuevaCita.correoPadreFamilia,
+                subject: 'Se ha agendado su cita',
+                html: `<html LANG="ES">
+                    <head>
+                        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+                        <style>
+                            .wrapper{
+                            background : #81ecec;
+                            font-family: 'Roboto', sans-serif;
+                            }
+                            .container{
+                            margin: 0 auto;
+                            background: #fff;
+                            width: 500px;
+                            text-align: center;
+                            padding: 10px;
+                            }
+                            .boton{
+                            background: #ff7675;
+                            color: #fff;
+                            display: block;
+                            padding: 15px;
+                            text-decoration: none;
+                            width: 50%;
+                            margin: 0 auto;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                      <div class="container">
+                        <h1>Ha agendado su cita con ${nuevaCita.nombrePadreFamilia}</h1>
+                      <h2>Su biblioteca digital</h2>
+
+                      <p>Saludos ${nuevaCita.nombrePadreFamilia} le agradecemos por escoger utilizar los servicios de SICEN</p>
+                      <p>El correo electrónico asociado es: ${nuevoUsuario.correo}</p>
+                      <p>Su contraseña temporal es: ${nuevoUsuario.contrasenna}</p>
+                      <p>Para ingresar visite el siguiente<p>
+                        <a href="http://localhost:3000/public/inicio_sesion.html" class="boton">Ingresar a Crux </a>
+                      </div>
+
+                    </body>
+
+                  </html>`
+            };
+* */
